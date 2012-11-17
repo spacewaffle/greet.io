60a3e010b95452ce3ed41010cb056c34b9965387
JonCheng <jon.syndicate@gmail.com>
2012-11-17 13:11:52 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GreetIo::Application.config.secret_token = '618a995644be88a0e7d656043ea895be1ca092d485782a7dd48d88b05541788bd41356f88372696ee4a6fe3325be8b97842f5374d804e132fc584f82cf9c44f1'
