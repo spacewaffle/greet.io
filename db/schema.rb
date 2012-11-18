d3d1b6179d4e865154b059df51ff5e440484078d
Jiyeon Woo <jiyeon8497@gmail.com>
2012-11-17 20:45:50 -0500

    changed views and db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121117222831) do
+ActiveRecord::Schema.define(:version => 20121117224839) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -30,8 +30,12 @@ ActiveRecord::Schema.define(:version => 20121117222831) do
   create_table "user_at_events", :force => true do |t|
     t.string   "name"
     t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", :force => true do |t|
